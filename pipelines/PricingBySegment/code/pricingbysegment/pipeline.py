2d1fdd881d4e7c60a5d9c035f0c17c1d10a2e21b
anya.bida1@gmail.com <anya.bida1@gmail.com>
2023-04-19 04:36:19 +0000

    ❇️ New     - Dataset        - ShippingReportSegmented
    🔁 Modified
        - Pipeline
           - ShippingBySegment

--- pipelines/PricingBySegment/code/pricingbysegment/pipeline.py
+++ pipelines/PricingBySegment/code/pricingbysegment/pipeline.py
@@ -14,9 +14,8 @@ def pipeline(spark: SparkSession) -> None:
     df_AdjustCols = AdjustCols(spark, df_ByOrderkey)
     df_AdjustCols = df_AdjustCols.cache()
     df_BySegment = BySegment(spark, df_AdjustCols)
-    df_FoodOrders = FoodOrders(spark, Config.FoodOrders)
     df_SumAmounts = SumAmounts(spark, df_BySegment)
-    Pricing(spark, df_SumAmounts)
+    ShippingReportSegmented(spark, df_SumAmounts)
 
 def main():
     spark = SparkSession.builder\
